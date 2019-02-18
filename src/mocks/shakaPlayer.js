984fb2a28fe6a78b4a16eec866c2c4b90cdd3875
Usamedin <usamedin.nuhiji@massive.co>
2019-02-18 14:30:42 +0100

    Minimal OTT player

---
+++ src/mocks/shakaPlayer.js
@@ -0,0 +1,12 @@
+class SimpleTextDisplayer {}
+export default {
+  text: {
+    SimpleTextDisplayer
+  },
+  polyfill: {
+    installAll() {}
+  },
+  Player: {
+    isBrowserSupported() {}
+  }
+};
